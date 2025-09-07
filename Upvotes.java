1e46ac0b89ea97b3b732bd04b058b7f06c72b63e
vaalss <valeria.hernandezmaldo@gmail.com>
2025-09-07 16:27:04 -0600

    agregar atributos en clase Upvotes

--- Upvotes.java
+++ Upvotes.java
@@ -1,3 +1,8 @@
+import java.time.LocalDate;
+
 public class Upvotes {
-    
+    private Catedrático catedratico;
+    private Estudiante estudiante;
+    private Curso curso;
+    private LocalDate fechas;
 }
