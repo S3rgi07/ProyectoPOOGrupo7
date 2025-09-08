3f97ca4b9ddd0b947bd2db9ed532f1c1f9692056
its-tony <tonycontacto361@gmail.com>
2025-09-08 17:49:11 -0600

    Empezé a trabajar en estado de cursos

--- EstadoCursos.java
+++ EstadoCursos.java
@@ -1,3 +1,34 @@
-public class EstadoCursos{
-    hola prueba
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstadoCursos {
+
+    // atributos
+    private List<Curso> cursosActuales;
+    private List<Curso> cursosHistoria;
+
+    // constructor
+    public EstadoCursos() {
+        this.cursosActuales = new ArrayList<>();
+        this.cursosHistoria = new ArrayList<>();
+    }
+
+    // get y setters
+    public List<Curso> getCursosActuales() {
+        return cursosActuales;
+    }
+
+    public void setCursosActuales(List<Curso> cursosActuales) {
+        this.cursosActuales = cursosActuales;
+    }
+
+    public List<Curso> getCursosHistoria() {
+        return cursosHistoria;
+    }
+
+    public void setCursosHistoria(List<Curso> cursosHistoria) {
+        this.cursosHistoria = cursosHistoria;
+    }
 }
