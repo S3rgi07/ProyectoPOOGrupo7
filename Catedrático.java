3012ee5cc0cb354293c89a93782755425238b88a
daniMont06 <danimontenegrodelaroca@gmail.com>
2025-09-08 13:55:29 -0600

    Getters de catedratico

--- Catedrático.java
+++ Catedrático.java
@@ -3,9 +3,17 @@ public class Catedrático {
     private String descripcion;
     private int upvotesCatedratico;
 
-    public Catedratico(String nombre, String descripcion, int upvotesCatedratico){
+    public Catedratico(String nombre, String descripcion, int upvotesCatedratico){ //Constructor
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.upvotesCatedratico = 0;
+        this.upvotesCatedratico = upvotesCatedratico;
+    }
+
+    public String getNombre(){ //Métodos
+        return nombre;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
     }
 }
