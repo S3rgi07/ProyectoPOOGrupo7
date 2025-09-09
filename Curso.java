ee5ab7874f06fb37e2fca145661f3bf3d271c734
daniMont06 <danimontenegrodelaroca@gmail.com>
2025-09-09 12:49:01 -0600

    Contructor de curso

--- Curso.java
+++ Curso.java
@@ -5,4 +5,13 @@ public class Curso {
     private String competencias;
     private String tipo;
     private Upvotes upvotesCurso;
+
+    public Curso(int codigo, String nombre, String descripcion, String competencias, String tipo, Upvotes upvotesCurso){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.competencias = competencias;
+        this.tipo = tipo;
+        this.upvotesCurso = upvotesCurso;
+    }
 }
