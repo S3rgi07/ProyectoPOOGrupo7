c22da6d8cfc64c715322907c372e2868cdbb432c
daniMont06 <danimontenegrodelaroca@gmail.com>
2025-09-09 12:17:01 -0600

    Propiedades de clase curso

--- Curso.java
+++ Curso.java
@@ -1,3 +1,8 @@
 public class Curso {
-    
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private String competencias;
+    private String tipo;
+    private Upvotes upvotesCurso;
 }
