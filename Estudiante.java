1221375478d7e6c638920f91085d647ab29b03ef
its-tony <tonycontacto361@gmail.com>
2025-09-08 19:05:25 -0600

    Empece hacer Estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -1,3 +1,47 @@
+
 public class Estudiante {
-    
+
+    // atributos
+    private String nombre;
+    private int carnet;
+    private String correo;
+    private EstadoCursos estadoCursos;
+
+    // construct
+    public Estudiante() {
+        this.estadoCursos = new EstadoCursos();
+    }
+
+    // get y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public EstadoCursos getEstadoCursos() {
+        return estadoCursos;
+    }
+
+    public void setEstadoCursos(EstadoCursos estadoCursos) {
+        this.estadoCursos = estadoCursos;
+    }
 }
