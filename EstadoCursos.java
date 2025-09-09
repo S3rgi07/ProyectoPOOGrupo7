195392dc30b432f0bc57366982638032c2ab8b4e
its-tony <tonycontacto361@gmail.com>
2025-09-08 18:05:55 -0600

    Comprobaciones

--- EstadoCursos.java
+++ EstadoCursos.java
@@ -1,5 +1,3 @@
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,5 +28,32 @@ public class EstadoCursos {
 
     public void setCursosHistoria(List<Curso> cursosHistoria) {
         this.cursosHistoria = cursosHistoria;
+    }
+
+    // metodos
+    public void iniciarCurso(Curso curso) {
+        if (curso != null) {
+            cursosActuales.add(curso);
+        }
+    }
+
+    
+    //  Mueve un curso de actuales a historia, indicando que se ha finalizado
+
+    public void finalizarCurso(Curso curso) {
+        if (curso != null && cursosActuales.contains(curso)) {
+            cursosActuales.remove(curso);
+            cursosHistoria.add(curso);
+        }
+    }
+
+//  va a verificar si un curso está ya está en la lista de actuales
+    
+    public boolean estaCursando(Curso curso) {
+        return cursosActuales.contains(curso);
+    }
+// va a verificar si un curso ya fue finalizado y si está en la historia
+    public boolean yaCursado(Curso curso) {
+        return cursosHistoria.contains(curso);
     }
 }
