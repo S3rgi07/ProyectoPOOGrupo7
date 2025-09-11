9737633a8fdbe83f52c4743859e19fe7cb12957b
S3rgi07 <sergiodaniel007@gmail.com>
2025-09-10 20:37:12 -0600

    Agregación de librerías necesarias para conexión con SQL

--- ConexionUVRate.java
+++ ConexionUVRate.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+
 public class ConexionUVRate {
+    
     
 }
