44cf0cde9c8e5932ef865a3a6ec751c62a7f5211
gaboo05 <cargabrielhernandez@gmail.com>
2025-09-10 22:35:41 -0600

    Agregamos el metodo sugerirCursos

--- Orientador.java
+++ Orientador.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 
 // Creamos 2 listas: un listado de metas y otro listado de cursos
 public class Orientador {
     private List<Meta> metas;
     private List<Curso> cursos;
+
+// Constructor
+public Orientador() {
+    this.metas = new ArrayList<>();
+    this.cursos = new ArrayList<>();
+    }
+
+// Método que sugiere los cursos al estudiante según la meta que tenga.
+public List<Curso> sugerirCursos(String metaNombre) {
+    List<Curso> sugeridos = new ArrayList<>();
+    }
 }
