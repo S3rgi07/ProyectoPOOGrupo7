939a839949d75ea46594ebab5d8bba9f67f68a24
gaboo05 <cargabrielhernandez@gmail.com>
2025-09-10 22:30:49 -0600

    Creamos la clase Orientador y las dos listas con las que vamos a trabajar

--- Orientador.java
+++ Orientador.java
@@ -1,2 +1,7 @@
+import java.util.List;
+
+// Creamos 2 listas: un listado de metas y otro listado de cursos
 public class Orientador {
+    private List<Meta> metas;
+    private List<Curso> cursos;
 }
