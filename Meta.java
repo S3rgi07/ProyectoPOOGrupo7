501eb193287400d06e29f553c4861a88675b4564
gaboo05 <cargabrielhernandez@gmail.com>
2025-09-10 22:05:38 -0600

    Agregamos el constructor para la clase Meta

--- Meta.java
+++ Meta.java
@@ -1,4 +1,10 @@
 public class Meta {
     private String nombre;
     private String competencias;
+
+    // Constructor
+    public Meta (String nombre, String competencias) {
+        this.nombre = nombre;
+        this.competencias = competencias;
+    }
 }
