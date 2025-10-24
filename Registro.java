01706dc417cc859d0a622e58adde3a0205181bd3
vaalss <valeria.hernandezmaldo@gmail.com>
2025-10-23 19:51:35 -0600

    componentes y etiquetas para registrar

--- Registro.java
+++ Registro.java
@@ -1,4 +1,15 @@
 public class Registro implements ActionListener {
 
-   
+    //Componentes principales
+    JFrame frame = new JFrame ("Registrar");
+    JButton registrarBoton = new JButton("Registrar");
+    JTextField usuarioID = new JTextField();
+    JPasswordField userpassword = new JPasswordField();
+    JPasswordField userpassword2 = new JPasswordField();
+
+    //Etiquetas
+    JLabel passwordLabel = new JLabel("Ingrese clave:");
+    JLabel passwordLabel2 = new JLabel("Confirmar clave:");
+    JLabel messageLabel = new JLabel();
+    
 }
