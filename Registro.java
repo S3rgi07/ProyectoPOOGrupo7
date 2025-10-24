e2ce0a762cddda991169acbfda7ca0a60df490f7
vaalss <valeria.hernandezmaldo@gmail.com>
2025-10-23 20:03:04 -0600

    posiciones para las etiquetas y botones

--- Registro.java
+++ Registro.java
@@ -11,5 +11,23 @@ public class Registro implements ActionListener {
     JLabel passwordLabel = new JLabel("Ingrese clave:");
     JLabel passwordLabel2 = new JLabel("Confirmar clave:");
     JLabel messageLabel = new JLabel();
-    
+
+    public Registro() {
+
+        // Posiciones y tamaños de los componentes (x, y, ancho, alto)
+        usuarioLabel.setBounds(50, 80, 200, 25);
+        usuarioID.setBounds(220, 80, 150, 25);
+
+        passwordLabel.setBounds(50, 120, 200, 25);
+        userpassword.setBounds(220, 120, 150, 25);
+
+        passwordLabel2.setBounds(50, 160, 200, 25);
+        userpassword2.setBounds(220, 160, 150, 25);
+
+        registrarBoton.setBounds(150, 210, 100, 30);
+        registrarBoton.addActionListener(this); //detecta clicks en el botón
+
+        messageLabel.setBounds(100, 260, 250, 35);
+        messageLabel.setFont(new Font("Arial", Font.ITALIC, 14));
+        messageLabel.setHorizontalAlignment(SwingConstants.CENTER); //centra el texto horizontalmente dentro del botón
 }
