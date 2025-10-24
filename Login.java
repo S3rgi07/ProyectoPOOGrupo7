f6b0d099018ac65055d887915e6552a1c3b17a55
daniMont06 <danimontenegrodelaroca@gmail.com>
2025-10-23 18:50:28 -0600

    especificaciones para la interfaz de login graficas

--- Login.java
+++ Login.java
@@ -4,3 +4,18 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*; //Importa toda la libreria
 
+public class Login implements ActionListener { //el action Listener "escucha" lo que el usuario hace, lo vamos a usar para los botones
+	
+	JFrame frame = new JFrame();
+	JButton loginboton = new JButton("Login");
+	JButton resetbuton = new JButton("Reset");
+	JButton registroboton = new JButton("Registrate");
+	JTextField userID = new JTextField();
+	JPasswordField userpassword = new JPasswordField();
+	JLabel userLabel = new JLabel("userID: ");
+	JLabel passwordLabel = new JLabel("password: ");
+	JLabel messageLabel = new JLabel();
+	
+}
+
+
