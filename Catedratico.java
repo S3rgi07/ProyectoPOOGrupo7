ef528f8c7cb9160f1f5de3f03e199e9993ac6cb0
gaboo05 <cargabrielhernandez@gmail.com>
2025-10-24 01:25:12 -0600

    Se agrego el ArrayList que contiene los cursos a la clase Catedratico

--- Catedratico.java
+++ Catedratico.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Catedratico {
     private String nombre;
     private String descripcion;
@@ -23,5 +25,14 @@ public class Catedratico {
 
     public void setUpvotesCat(Upvotes upvotesCatedratico) {
     this.upvotesCatedratico = upvotesCatedratico;
+    }
+
+    // Lista que contiene los cursos
+    public ArrayList<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(ArrayList<Curso> cursos) {
+        this.cursos = cursos;
     }
 }
