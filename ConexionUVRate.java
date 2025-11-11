9ba6839d94b4b6415b754484e027cc55f9de8a6f
daniMont06 <danimontenegrodelaroca@gmail.com>
2025-11-10 22:12:30 -0600

    El Método para prprobar conexión

--- ConexionUVRate.java
+++ ConexionUVRate.java
@@ -4,6 +4,28 @@ import java.sql.SQLException;
 
 public class ConexionUVRate {
 
+    // Ruta relativa al archivo SQLite dentro del proyecto
+    private static final String DB_URL = "jdbc:sqlite:database/uvrate_db.sqlite";
+    public static Connection getConnection() {
+    Connection conn = null;
+    try {
+        // Cargar el driver JDBC de SQLite
+        Class.forName("org.sqlite.JDBC");
 
-// Ruta relativa al archivo SQLite dentro del proyecto
-private static final String DB_URL = "jdbc:sqlite:database/uvrate_db.sqlite";
+        // Intentar conexión
+        conn = DriverManager.getConnection(DB_URL);
+        System.out.println("[OK] Conectado a la base de datos SQLite correctamente.");
+
+    } catch (ClassNotFoundException e) {
+        System.err.println("[ERROR] No se encontró el driver JDBC de SQLite.");
+        e.printStackTrace();
+
+    } catch (SQLException e) {
+        System.err.println("[ERROR] Falló la conexión a la base de datos SQLite.");
+        e.printStackTrace();
+    }
+
+    return conn;
+    }
+
+}
